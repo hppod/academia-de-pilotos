9343a3619cb705e06442c6594dea3a144f3fe3e1
hppod <hppodgornik@gmail.com>
2018-10-29 00:47:51 -0300

    criando uma função que define se um carro é rapido ou não e utilizando template string no log quando aplicamos a função

--- app.ts
+++ app.ts
@@ -4,3 +4,10 @@ console.log(message)
 let meuCarroFavorito
 meuCarroFavorito = 'Porsche'
 console.log("Meu carro favorito é um " + meuCarroFavorito)
+
+let carroRapido = function (velocidade: number): boolean {
+    return velocidade > 200
+}
+
+let velocidadeFinal = 250
+console.log(`A velocidade final ${velocidadeFinal} é suficiente para ele ser considerado um carro rápido? ${carroRapido(velocidadeFinal) ? 'SIM' : 'NÃO'}`)
--- dist/app.js
+++ dist/app.js
@@ -3,3 +3,8 @@ console.log(message);
 var meuCarroFavorito;
 meuCarroFavorito = 'Porsche';
 console.log("Meu carro favorito é um " + meuCarroFavorito);
+var carroRapido = function (velocidade) {
+    return velocidade > 200;
+};
+var velocidadeFinal = 250;
+console.log("A velocidade final " + velocidadeFinal + " \u00E9 suficiente para ele ser considerado um carro r\u00E1pido? " + (carroRapido(velocidadeFinal) ? 'SIM' : 'NÃO'));
