6b4cda84bdce634ebe4840f52618c28c8ebb1ec4
hppod <hppodgornik@gmail.com>
2018-10-29 00:44:22 -0300

    criando variaveis não tipadas

--- app.ts
+++ app.ts
@@ -1,2 +1,6 @@
 let message: string = "Hello World"
 console.log(message)
+
+let meuCarroFavorito
+meuCarroFavorito = 'Porsche'
+console.log("Meu carro favorito é um " + meuCarroFavorito)
--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,5 @@
 var message = "Hello World";
 console.log(message);
+var meuCarroFavorito;
+meuCarroFavorito = 'Porsche';
+console.log("Meu carro favorito é um " + meuCarroFavorito);
